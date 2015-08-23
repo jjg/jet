34a84cecd7961ae851c72c7a0e93db868149be50
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-08-22 22:41:53 -0500

    fleshing out methods

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
+var https = require("https");
+var crypto = require("crypto");
+var url = require("url");
 var config = require("./config.js");
 var log = require("./jlog.js");
 log.level = config.LOG_LEVEL;
 
 log.message(log.INFO, "jet starting up...");
+
+// todo: determine HTTP method
+
+// todo: handle OPTIONS request
+
+// todo: reject all other HTTP method requests
+
+// todo: handle GET request
+
+// todo: generate hash of relevant request parameters (URL, headers, etc.)
+
+// todo: check cache for request hash
+
+// todo: create new cache entry
+
+// todo: relay request to origin server
+//	do not forwart RANGE request parameters
+
+// todo: initialize cache entry metadata using origin server response headers
+
+// todo: update cache entry metadata:
+//  measure inbound datarate from origin server
+//  and throttle client response to maintain
+//  buffer underrun
+
+// todo: write bytes from origin server to client request
+
+// todo: write bytes from origin server to cache
+
+// todo: close origin server connection
+
+// todo: close client request connection
