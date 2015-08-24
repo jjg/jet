e6df6dbdc5883c61780c228dd9006fdb155181c3
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-08-24 16:29:06 -0500

    config tweaks

--- config.ex
+++ config.ex
@@ -1,7 +1,8 @@
 module.exports = {
-	LOG_LEVEL: 0,
+	LOG_LEVEL: 1,
 	SERVER_PORT: 5000,
 	ORIGIN_SERVER_HOST: "localhost",
 	ORIGIN_SERVER_PORT: 6000,
-	CACHE_EXPIRATION: 1	// minutes
+	CACHE_EXPIRATION: 60,	// minutes
+	MAXIMUM_CACHE_SIZE: 50	// megabytes
 };
--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
 	SERVER_PORT: 5000,
 	ORIGIN_SERVER_HOST: "localhost",
 	ORIGIN_SERVER_PORT: 6000,
-	CACHE_EXPIRATION: 5,	// minutes
+	CACHE_EXPIRATION: 60,	// minutes
 	MAXIMUM_CACHE_SIZE: 50	// megabytes
 };
