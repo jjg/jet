17402bfa61e26f59a5e8f11bdcadfb69c6fb4038
Jason J. Gullickson <mr@jasongullickson.com>
2025-08-08 07:38:22 -0500

    (over)writing to a single journal file.

--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
 	// TODO: Look for settings (~/.config/jet/settings.json)
 
-	// TODO: Draw ruler
-	fmt.Println("|--------|---------|---------|---------|---------|---------|---------|---------|")
+	// Draw ruler
+	fmt.Println("  |--------|---------|---------|---------|---------|---------|---------|---------|")
 
-	// TODO: Read input
+	// Read input
+	entry := make([]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		text := scanner.Text()
 
-	// TODO: Listen for EOF
+		// Listen for EOF
+		if len(text) != 0 {
+			entry = append(entry, text)
+		} else {
+			break
+		}
+	}
+
+	// DEBUG: Dump entry to console
+	fmt.Println(entry)
+
+	// TODO: Seems like `entry` might be missing the linefeeds...
 
 	// TODO: Check for existing journal file for today
 
 	// TODO: Create or update today's journal file
+	f, err := os.Create("journal/today.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
+	w := bufio.NewWriter(f)
+	for _, line := range entry {
+
+		// TODO: See if there's a better way to add this linefeed
+		formattedLine := fmt.Sprintf("%s\n", line)
+
+		_, err := w.WriteString(formattedLine)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	w.Flush()
 }
