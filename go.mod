a89db005eccda527e51a10da67b2733c99e9e0a3
Jason J. Gullickson <mr@jasongullickson.com>
2025-08-08 07:10:17 -0500

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module code.kratzgullicksonlabs.com/json/jet
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// TODO: Look for settings (~/.config/jet/settings.json)
+
+	// TODO: Draw ruler
+	fmt.Println("|--------|---------|---------|---------|---------|---------|---------|---------|")
+
+	// TODO: Read input
+
+	// TODO: Listen for EOF
+
+	// TODO: Check for existing journal file for today
+
+	// TODO: Create or update today's journal file
+
+}
