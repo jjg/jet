a1a864483cd7e292942d25ccbab2edb7a3d4004f
Jason J. Gullickson <mr@jasongullickson.com>
2025-08-13 15:55:17 -0500

    Start sketching.

---
+++ tools/hop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("this is hop")
+
+	// TODO: Import any incomplete items from yesterday's list.
+	// TODO: Try to open today's file and if it doesn't exist, create it.
+	// TODO: Extract the hop list if there is one.
+	// TODO: Display the list
+	// TODO: Prompt for command (add/remove/complete/quit)
+	// TODO: Add an item to the list
+	// TODO: Remove an item from the list
+	// TODO: Mark an item completed
+	// TODO: Quit (begin shutdown process)
+	// TODO: Save changes
+	// TODO: Quit
+}
