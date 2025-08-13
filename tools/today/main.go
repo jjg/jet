55db992753d3a11e572cdd2512577fb03294572e
Jason J. Gullickson <mr@jasongullickson.com>
2025-08-13 09:28:17 -0500

    Refactor today.

--- tools/today/main.go
+++ tools/today/main.go
@@ -2,35 +2,18 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"os"
 	"time"
 )
 
 func getJournalDir() string {
-
-	// Make sure we have a working journal dir before they write anything.
 	home, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
-	journalDir := fmt.Sprintf("%s/jet-journal", home)
-
-	// Check if journal dir exists and if not, create it.
-	_, err = os.Stat(journalDir)
-	if errors.Is(err, fs.ErrNotExist) {
-
-		// Try to create the journal dir.
-		// TODO: Maybe notify (and confirm?) before doing this?
-		if err := os.MkdirAll(journalDir, 0700); err != nil {
-			panic(err)
-		}
-	}
-
-	return journalDir
+	return fmt.Sprintf("%s/jet-journal", home)
 }
 
 func showEntry(journalDir string, entryName string) {
@@ -57,8 +40,6 @@ func showEntry(journalDir string, entryName string) {
 func main() {
 	t := time.Now()
 	journalDir := getJournalDir()
-
-	fmt.Println("its today")
 	entryName := t.Format("2006-01-02")
 	showEntry(journalDir, entryName)
 }
