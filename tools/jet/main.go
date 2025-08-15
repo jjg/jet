1136cf4caa9183c0811daa26eee19afdc8eecca7
Jason J. Gullickson <mr@jasongullickson.com>
2025-08-15 13:15:06 -0500

    Jet working with the pouch-style data directory.

--- tools/jet/main.go
+++ tools/jet/main.go
@@ -10,14 +10,52 @@ import (
 	"time"
 )
 
-func drawHeader() {
+func getDataDir(t time.Time) string {
+	// Make sure we have a working data dir.
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	dataDir := fmt.Sprintf("%s/pouch-data/%s", home, t.Format("2006-01-02"))
+	// Check if data dir exists and if not, create it.
+	_, err = os.Stat(dataDir)
+	if errors.Is(err, fs.ErrNotExist) {
+		// Try to create the datadir.
+		if err := os.MkdirAll(dataDir, 0700); err != nil {
+			panic(err)
+		}
+	}
+	return dataDir
+}
 
+func storeEntry(entry []string, t time.Time) {
+	dataDir := getDataDir(t)
+	// Create or update the file for the specified journal entry.
+	filename := fmt.Sprintf("%s/jet.txt", dataDir)
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	// Write it to disk.
+	w := bufio.NewWriter(f)
+	for _, line := range entry {
+		if _, err := w.WriteString(line); err != nil {
+			panic(err)
+		}
+		if _, err := w.WriteString("\n"); err != nil {
+			panic(err)
+		}
+	}
+	w.Flush()
+}
+
+func drawHeader() {
 	// Get the terminal dimensions (ignore height since we don't use it).
 	termWidth, _, err := term.GetSize(0)
 	if err != nil {
 		panic(err)
 	}
-
 	// Draw ruler.
 	rulerWidth := termWidth - 4
 	fmt.Print("  |")
@@ -32,47 +70,19 @@ func drawHeader() {
 	fmt.Print("|\n")
 }
 
-func getJournalDir() string {
-
-	// Make sure we have a working journal dir before they write anything.
-	home, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-	journalDir := fmt.Sprintf("%s/jet-journal", home)
-
-	// Check if journal dir exists and if not, create it.
-	_, err = os.Stat(journalDir)
-	if errors.Is(err, fs.ErrNotExist) {
-
-		// Try to create the journal dir.
-		// TODO: Maybe notify (and confirm?) before doing this?
-		if err := os.MkdirAll(journalDir, 0700); err != nil {
-			panic(err)
-		}
-	}
-
-	return journalDir
-}
-
 func getInput(interactive bool) []string {
-
 	if interactive {
 		drawHeader()
 	}
-
 	entry := make([]string, 0)
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-
 		if interactive {
 			fmt.Print("> ")
 		}
-
 		more := scanner.Scan()
 		text := scanner.Text()
 		entry = append(entry, text)
-
 		// Break on double linefeed if in interactive mode.
 		if interactive {
 			if len(text) == 0 {
@@ -84,50 +94,23 @@ func getInput(interactive bool) []string {
 			}
 		}
 	}
-
 	return entry
 }
 
-func storeEntry(journalDir string, entryName string, entry []string) {
-
-	// Create or update the file for the specified journal entry.
-	filename := fmt.Sprintf("%s/%s.jet.txt", journalDir, entryName)
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	// Write it to disk.
-	w := bufio.NewWriter(f)
-	for _, line := range entry {
-		if _, err := w.WriteString(line); err != nil {
-			panic(err)
-		}
-		if _, err := w.WriteString("\n"); err != nil {
-			panic(err)
-		}
-	}
-	w.Flush()
-}
-
 func main() {
-	t := time.Now()
-	journalDir := getJournalDir()
-
+	today := time.Now()
 	// Look for subcommands on the command line.
 	subCommand := "newInteractiveEntry"
 	args := os.Args
 	if len(args) > 1 {
 		subCommand = args[1]
 	}
-
 	switch subCommand {
 	case "help":
 		fmt.Println("A reliable focused writing tool.")
 		fmt.Println("")
-		fmt.Println("Journal entries for each day will be stored in a directory")
-		fmt.Println("named 'jet-journal' in your home directory.")
+		fmt.Println("Journal entries for each day will be stored in a subdirectory")
+		fmt.Println("in the 'pouch-data' in your home directory.")
 		fmt.Println("")
 		fmt.Println("Usage:")
 		fmt.Println("jet - Create a new journal entry in interactive mode")
@@ -137,10 +120,8 @@ func main() {
 		fmt.Println("today, yesterday")
 		fmt.Println("")
 	default:
-
 		// If no subcommand is provided, create a new entry.
-		entryName := t.Format("2006-01-02")
 		entry := getInput(term.IsTerminal(0))
-		storeEntry(journalDir, entryName, entry)
+		storeEntry(entry, today)
 	}
 }
